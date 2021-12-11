77caf345826911e11ea6d01f7b49f08448390791
BenWalls <ruben.walangare@gmail.com>
2021-12-11 14:31:23 +0700

    Adding function to create div

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ container.classList.add('container');
 containerTwo.classList.add('containerTwo');
 
 container.textContent = "this container";
-containerTwo.textContent = "this container two";
 
 theBody.appendChild(container);
 container.appendChild(containerTwo);
 
+/*
 function grid16() {
 for(let i = 0; i < 16; i++) {
     containerTwo.appendChild(document.createElement('div'));
@@ -18,7 +18,19 @@ for(let i = 0; i < 16; i++) {
 }
 
 grid16();
+*/
+containerTwo.setAttribute('style', 'display: flex; flex-wrap: wrap; margin: auto; padding: 1px; align-content: center; width: 700px; height: 700px')
 
-const divAll = document.querySelectorAll('div');
+// the function below is proper way to create variable with loop. I utilize
+// this method to create numbers of div by manipulate the DOM 
 
-divAll.setAttribute('style', 'border: 1px, solid, black');
+function gridNum(sum) {
+    for (let i = 0; i < sum; i++) {
+        this['grid' + i] = document.createElement('div');
+        this['grid' + i].classList.add('grid');
+        containerTwo.appendChild(this['grid' + i])
+        this['grid' + i].setAttribute('style', 'display: flex; margin: 0px; width: auto; height: auto; padding: 1px; border: 1px solid black');
+    };
+};
+
+gridNum(24);
