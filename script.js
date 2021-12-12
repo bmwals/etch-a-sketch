145a21a97d428b6f40905b1cafdf4b676dd660bd
BenWalls <ruben.walangare@gmail.com>
2021-12-12 12:37:31 +0700

    Implement responsive grid cells

--- script.js
+++ script.js
@@ -5,32 +5,27 @@ const containerTwo = document.createElement('div');
 container.classList.add('container');
 containerTwo.classList.add('containerTwo');
 
-container.textContent = "this container";
+container.textContent = "ETCH-A-SKETCH!";
 
 theBody.appendChild(container);
 container.appendChild(containerTwo);
 
-/*
-function grid16() {
-for(let i = 0; i < 16; i++) {
-    containerTwo.appendChild(document.createElement('div'));
-};
-}
-
-grid16();
-*/
-containerTwo.setAttribute('style', 'display: flex; flex-wrap: wrap; margin: auto; padding: 1px; align-content: center; width: 700px; height: 700px')
+// containerTwo.setAttribute('style', 'display: flex; flex-wrap: wrap; margin: auto; padding: 1px; align-content: center; width: 700px; height: 700px')
 
 // the function below is proper way to create variable with loop. I utilize
 // this method to create numbers of div by manipulate the DOM 
-
 function gridNum(sum) {
     for (let i = 0; i < sum; i++) {
-        this['grid' + i] = document.createElement('div');
-        this['grid' + i].classList.add('grid');
-        containerTwo.appendChild(this['grid' + i])
-        this['grid' + i].setAttribute('style', 'display: flex; margin: 0px; width: auto; height: auto; padding: 1px; border: 1px solid black');
+        grid = document.createElement('div');
+        grid.classList.add('grid');
+        containerTwo.appendChild(grid);
+        for (let j = 0; j < sum; j++) {
+            gridIn = document.createElement('div');
+            gridIn.classList.add('gridIn');
+            grid.appendChild(gridIn);
+        }
+    //    this['grid' + i].setAttribute('style', 'width: 600/16px; height: 600/16px');
     };
 };
 
-gridNum(24);
+gridNum(10);
