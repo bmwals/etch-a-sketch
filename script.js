01dc90c222121df92afd3b06b87c7526abd1dc39
BenWalls <ruben.walangare@gmail.com>
2021-12-13 13:24:01 +0700

    Fix set grid number behaviour

--- script.js
+++ script.js
@@ -3,24 +3,32 @@ const theBody = document.body; // best way than querySelector to select the html
 const container = document.createElement('div');
 const containerTwo = document.createElement('div');
 const setGrid = document.createElement('button');
+const resetGrid = document.createElement('button');
 setGrid.setAttribute('id', 'setGrid');
 setGrid.setAttribute('type', 'button');
-setGrid.setAttribute('onClick', "gridPromp()")
+resetGrid.setAttribute('id', 'resetGrid');
+resetGrid.setAttribute('type', 'button');
+//resetCell.setAttribute('onClick', "gridPromp()")
 container.classList.add('container');
 containerTwo.classList.add('containerTwo');
 
 container.textContent = "ETCH-A-SKETCH!";
 setGrid.textContent = "Set Grid Number";
+resetGrid.textContent = "Reset Grid";
 
 theBody.appendChild(container);
 container.appendChild(setGrid);
+container.appendChild(resetGrid);
 container.appendChild(containerTwo);
+
 
 // containerTwo.setAttribute('style', 'display: flex; flex-wrap: wrap; margin: auto; padding: 1px; align-content: center; width: 700px; height: 700px')
 
 // the function below is proper way to create variable with loop. I utilize
 // this method to create numbers of div by manipulate the DOM 
+
 function gridNum(sum) {
+    containerTwo.replaceChildren();
     for (let i = 0; i < sum; i++) {
         grid = document.createElement('div');
         grid.classList.add('grid');
@@ -39,12 +47,16 @@ function gridPrompt() {
     let sum = prompt('Please enter cells x cells', '32');
     if (sum > 100 || sum < 1 || sum > 0 === false && sum <= 100 === false) {
         alert('Please enter number between 1 to 100. Applying 32x32 cells...');
-        sum = 32;
+        return sum = 0;
     };
     return sum;
-}
+};
 
-let sum = gridPrompt()
+setGrid.addEventListener('click', () => {
+    sum = gridPrompt();
+    gridNum(sum);
+});
 
-gridNum(sum);
-
+resetGrid.addEventListener('click', () => {
+    containerTwo.replaceChildren(); // this stuff removes whole child nodes
+});
